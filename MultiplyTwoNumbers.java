78beda9dfc428c721754a8172095981671f7f417
Rajyavardhan Bhandari <112614495+RajyavardhanBhandari@users.noreply.github.com>
2023-09-08 10:15:20 +0530

    next commit

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter the first number
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        // Prompt the user to enter the second number
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Calculate the product of the two numbers
+        double product = num1 * num2;
+
+        // Display the result
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+
+        // Close the scanner
+        scanner.close();
+    }
+}
